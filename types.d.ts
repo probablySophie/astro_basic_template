828d5e771804f8a92386d26fbf3511d806571908
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-04-26 12:59:06 +0800

    types

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+
+export declare type Frontmatter = {
+	title?: string,
+	date?: string,
+	author?: string,
+	summary?: string
+}
+
+export declare type Config = {
+	favicon?: string
+	baseSiteName?: string
+	webManifest?: string
+}
