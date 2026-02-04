e16f55274a86476dc6b51a33a562207a973af96a
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-02-04 16:10:03 +0800

    parameters

--- scripts/lazy_actions.ts
+++ scripts/lazy_actions.ts
@@ -1,3 +1,10 @@
+/**
+ * Actions: [ "show", "hide", "searchparam" ]
+ * Expectations:
+ * 		show -> has aria-controls
+ * 		hide -> has aria-controls
+ *		searchparam -> has data-value and data-param
+ */
 
 // Button actions
 document.addEventListener( "click", event =>
@@ -6,7 +13,25 @@ document.addEventListener( "click", event =>
 	if ( (event.target as HTMLElement)?.getAttribute("data-action") == null ) { return }
 	
 	let target = event.target as HTMLElement;
+	let action = target.getAttribute("data-action");
 	let return_val = false;
+
+	// Search params
+	switch ( action )
+	{
+		case "searchparam": {
+			let param = target.getAttribute("data-param");
+			if ( param == null ) { throw new Error( "data-action='searchparam' expects to have a data-param and optionally a data-value" ); }
+			let value = target.getAttribute("data-value");
+
+			const urlParams = new URLSearchParams(window.location.search);
+			if ( value == null ) { urlParams.delete( param ) }
+			else { urlParams.set( param, value ); }
+			// @ts-ignore
+			window.location.search = urlParams;
+			return true;
+		}
+	}
 
 	// For dialogs
 	action_elmtype_controls<HTMLDialogElement>( target, "show", "DIALOG",
