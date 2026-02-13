623c4817b0c134dc77b0b8b3bd49fdd9e29d7e5e
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-02-13 15:11:14 +0800

    forms?

---
+++ scripts/fancy_forms.ts
@@ -0,0 +1,50 @@
+/**
+ * @file Toggling forms 
+ * 
+ * The form should have either the class `show-inputs` or `hide-inputs` and should have a button inside with the class `toggle-inputs`.
+ *
+ * Elements can be hidden alongside inputs with the class `show-if-input-shown`
+ * Elements can be hidden when inputs are SHOWN with the class `hide-if-input-shown`
+ */
+
+/* INFO: You also want the css:
+form.hide-inputs input,
+form.hide-inputs select,
+form.hide-inputs textarea { display: none; }
+
+form.hide-inputs.hide-labels label { display: none; }
+
+form.hide-inputs .show-if-input-shown,
+form.show-inputs .hide-if-input-shown { display: none; }
+*/
+
+/* e.g.
+	<form class="hide-inputs">
+		<label class="show-if-input-shown">Please enter your name</label><input type="text" name="name" />
+		<p class="hide-if-input-shown">Click this button if you want to enter your name!</p>
+		<button class="toggle-inputs">Toggle Entering Name</button>
+		<button class="show-if-input-shown">Submit</button>
+	</form>
+ */
+
+document.addEventListener( "click", e => {
+	// Did we click a button?
+	if ( (e.target as HTMLElement)?.tagName != "BUTTON" ) { return }
+	// Does the button have the class `toggle-inputs`?
+	if ( (e.target as HTMLElement)?.classList.contains("toggle-inputs") != true ) { return }
+	// Are we a form's direct child?
+	if ( (e.target as HTMLElement)?.parentElement?.tagName != "FORM" ) { return }
+	
+	e.preventDefault();
+	// let b = (e.target as HTMLElement) as HTMLButtonElement;
+
+	let f = (e.target as HTMLElement).parentElement as HTMLFormElement;
+	if ( f.classList.contains( "hide-inputs" ) ) {
+		f.classList.remove( "hide-inputs" );
+		f.classList.add( "show-inputs" );
+	} else {
+		f.classList.remove( "show-inputs" );
+		f.classList.add( "hide-inputs" );
+	}
+	return true
+} )
