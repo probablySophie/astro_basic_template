fefbbb002cb36f9d13f96c4a3a5a559b610aeb33
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-02-15 16:29:58 +0800

    comments & a new utility function

--- scripts/lazy_actions.ts
+++ scripts/lazy_actions.ts
@@ -1,8 +1,7 @@
 /**
  * Actions: [ "show", "hide", "searchparam" ]
  * Expectations:
- * 		show -> has aria-controls
- * 		hide -> has aria-controls
+ * 		show, hide, toggle -> has aria-controls
  *		searchparam -> has data-value and data-param
  */
 
@@ -14,18 +13,17 @@ document.addEventListener( "click", event =>
 	
 	let target = event.target as HTMLElement;
 	let action = target.getAttribute("data-action");
-	let return_val = false;
 
 	// Search params
 	switch ( action )
 	{
 		case "searchparam": {
 			let param = target.getAttribute("data-param");
-			if ( param == null ) { throw new Error( "data-action='searchparam' expects to have a data-param and optionally a data-value" ); }
+			if ( param == null ) { throw new Error( "data-action='searchparam' expects the element to have a data-param and optionally a data-value" ); }
 			let value = target.getAttribute("data-value");
 
 			const urlParams = new URLSearchParams(window.location.search);
-			if ( value == null ) { urlParams.delete( param ) }
+			if ( value == null ) { urlParams.delete( param ); }
 			else { urlParams.set( param, value ); }
 			// @ts-ignore
 			window.location.search = urlParams;
@@ -34,25 +32,42 @@ document.addEventListener( "click", event =>
 	}
 
 	// For dialogs
-	action_elmtype_controls<HTMLDialogElement>( target, "show", "DIALOG",
-		e => { e.showModal(); return_val = true; }
-	);
-	action_elmtype_controls<HTMLDialogElement>( target, "hide", "DIALOG",
-		e => { e.close(); return_val = true; }
-	);
-	action_elmtype_controls<HTMLDialogElement>( target, "close", "DIALOG",
-		e => { e.close(); return_val = true; }
-	);
+	if ( action_elmtype_controls<HTMLDialogElement>( target, "show", "DIALOG",
+		e => { e.showModal(); return true; }
+	) ) { return true }
 
-	return return_val
+	if ( action_elmtype_controls<HTMLDialogElement>( target, ["hide", "close"], "DIALOG",
+		e => { close_dialog( e ); return true; }
+	) ) { return true }
+
+	if ( action_elmtype_controls<HTMLDialogElement>( target, "toggle", "DIALOG",
+		e => { if (e.open) { close_dialog( e ) } else { e.showModal() }; return true; }
+	) ) { return true }
+
+	return false
 });
 
-function action_elmtype_controls<T>( target: HTMLElement, action: string, controlled_tagname: string, callback: ( elm: T ) => void )
+
+function action_elmtype_controls<T>( target: HTMLElement, action: string | string[], controlled_tagname: string, callback: ( elm: T ) => boolean | void )
 {
-	if ( target.getAttribute("data-action") != action ) { return false }
+	let data_action = target.getAttribute("data-action");
+	// Null or string & not match
+	if ( data_action == null || ( typeof action == "string" && action != data_action ) ) { return }
+	// Array & not match
+	if ( Array.isArray( action ) && action.findIndex( a => a == data_action ) == -1 ) { return }	
+
+	let success = false;
 	for ( const controlled of Object.values( target.ariaControlsElements || [] ) )
 	{
 		if ( controlled.tagName != controlled_tagname ) { continue }
-		callback( controlled as T );
+		success = success || (callback( controlled as T ) || false);
 	}
+	return success
+}
+
+function close_dialog( elm: HTMLDialogElement )
+{
+	// https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/requestClose
+	if ( typeof elm.requestClose == "function" ) { elm.requestClose(); }
+	else { elm.close(); }
 }
